1dd8c3ffb4442f4057d495ec08cf85caf6ba22bf
Eduardo Uribe <eduardo.u24@gmail.com>
2021-01-05 00:35:31 -0300

    First set of UI examples, draggable window, image settings, atlas.

--- Scripts/Core/UI/BaseUIScript.cs
+++ Scripts/Core/UI/BaseUIScript.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace Anvil3D
 {
-    public class BaseUIScript : AnvilMonoBehaviour
+    public class BaseUIScript : AnvilMonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
-        [SerializeField] protected CanvasGroup m_rootCanvasGroup;
-        public CanvasGroup RootCanvasGroup => m_rootCanvasGroup;
 
-        [SerializeField] private bool m_hideOnAwake;
+        #region Base Config
+
+        private const string kBaseUIConfigGroupKey = "Base UI Settings";
+        private const int kBaseUIConfigGroupSort = -98;
+
+        private const string kShowHideButtonKey = "ShowHideButtons";
         
+        [SerializeField, FoldoutGroup(kBaseUIConfigGroupKey, order:kBaseUIConfigGroupSort)]
+        protected bool m_hideOnAwake;
+        
+        [SerializeField, FoldoutGroup(kBaseUIConfigGroupKey, order:kBaseUIConfigGroupSort)]
+        protected CanvasGroup m_rootCanvasGroup;
+
+        [SerializeField, FoldoutGroup(kBaseUIConfigGroupKey, order:kBaseUIConfigGroupSort), ReadOnly]
+        protected RectTransform m_rectTransform;
+        
+        public CanvasGroup RootCanvasGroup => m_rootCanvasGroup;
+        public RectTransform RectTransform => m_rectTransform;
+        
+        #endregion
+
+        #region Drag Options
+        
+        private const string kDragOptionsGroupKey = "Is Draggable";
+        private const string kDragOptionsGroupVariable = "m_isDraggable";
+        private const int kDragOptionsGroupSort = 201;
+
+        [Space]
+        [ToggleGroup(kDragOptionsGroupVariable, groupTitle:kDragOptionsGroupKey, order:kDragOptionsGroupSort)]
+        [SerializeField] private bool m_isDraggable = false;
+        
+        [ToggleGroup(kDragOptionsGroupVariable, groupTitle:kDragOptionsGroupKey, order:kDragOptionsGroupSort)]
+        [SerializeField] private bool m_moveToFrontOnDrag = false;
+
+        [ToggleGroup(kDragOptionsGroupVariable, groupTitle:kDragOptionsGroupKey, order:kDragOptionsGroupSort)]
+        [SerializeField] private UnityEvent m_onDragBegins;
+        
+        [ToggleGroup(kDragOptionsGroupVariable, groupTitle:kDragOptionsGroupKey, order:kDragOptionsGroupSort)]
+        [SerializeField] private UnityEvent m_onDrag;
+        
+        [ToggleGroup(kDragOptionsGroupVariable, groupTitle:kDragOptionsGroupKey, order:kDragOptionsGroupSort)]
+        [SerializeField] private UnityEvent m_onDragEnds;
+
+        #endregion
 
         protected virtual void Awake()
         {
@@ -20,6 +62,7 @@ namespace Anvil3D
             {
                 Hide();
             }
+            m_rectTransform = transform as RectTransform;
         }
 
         protected virtual void Start()
@@ -27,7 +70,7 @@ namespace Anvil3D
             
         }
 
-        [Button]
+        [Button(ButtonSizes.Medium), ButtonGroup(kShowHideButtonKey)]
         public virtual void Show()
         {
             if (!m_rootCanvasGroup.isActiveAndEnabled)
@@ -38,10 +81,49 @@ namespace Anvil3D
             m_rootCanvasGroup.DOFade(1f, 0f);
         }
         
-        [Button]
+        [Button(ButtonSizes.Medium), ButtonGroup(kShowHideButtonKey)]
         public virtual void Hide()
         {
             m_rootCanvasGroup.DOFade(0f, 0f).OnComplete(() => m_rootCanvasGroup.gameObject.SetActive(false));
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (!m_isDraggable)
+            {
+                return;
+            }
+            
+            m_onDrag?.Invoke();
+            
+            var anchoredPosition = m_rectTransform.anchoredPosition;
+            var newPosition = new Vector2(anchoredPosition.x + eventData.delta.x, anchoredPosition.y + eventData.delta.y);
+            m_rectTransform.anchoredPosition = newPosition;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (!m_isDraggable)
+            {
+                return;
+            }
+
+            if (m_moveToFrontOnDrag)
+            {
+                m_rectTransform.SetAsLastSibling();
+            }
+            
+            m_onDragBegins?.Invoke();
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (!m_isDraggable)
+            {
+                return;
+            }
+            
+            m_onDragEnds?.Invoke();
         }
     }
 }
